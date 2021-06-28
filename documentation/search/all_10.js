088bd450e632b15d7711e23592c5724c71384041
Matrax <alexandre.matrax@gmail.com>
2021-06-29 00:34:45 +0200

    Correction fautes d'orthographe doc

--- documentation/search/all_10.js
+++ documentation/search/all_10.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['readme_2emd_92',['README.md',['../README_8md.html',1,'']]],
   ['regenerate_5fpassword_2ephp_93',['regenerate_password.php',['../regenerate__password_8php.html',1,'']]],
-  ['render_94',['render',['../classView.html#ad6b9299348ecf7eb493628ed7f5af3c5',1,'View\render()'],['../classFrontController.html#a25d4d40ada87e8977676b3d9115c64db',1,'FrontController\render()']]]
+  ['render_94',['render',['../classFrontController.html#a25d4d40ada87e8977676b3d9115c64db',1,'FrontController\render()'],['../classView.html#ad6b9299348ecf7eb493628ed7f5af3c5',1,'View\render()']]]
 ];
